fbb34c2a757ad1b617428521709aa7b5567ad1bd
nowled <nowled@excite.com>
2021-01-03 15:13:45 +0300

    Moving On from Dashboard

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Page from '../../Shared/Page';
+import PriceGrid from './PriceGrid';
+
+const Dashboard = () => {
+  return (
+    <Page name='dashboard'>
+      <h1>Dashboard --Bitch!!</h1>
+      <PriceGrid />
+    </Page>
+  );
+};
+
+export default Dashboard;
